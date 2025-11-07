7df005e49dec198940c567f33e2d0f880f7c05f0
Soe Htun <soehtuncu@gmail.com>
2025-11-07 15:45:03 +0630

    add icon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <title>@yield('title', 'Todo App')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
+
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light mb-4">
